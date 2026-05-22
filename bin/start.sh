5ec07b3533ab8041ce97519f89ec57d288c260af
Mert Cingöz <mertcingoz@gmail.com>
2026-05-22 14:07:00 +0200

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -6,5 +6,6 @@ source bin/init.sh
 device=$1
 shift
 
-# emulator -avd pixel_9.0 -port 5558 -no-window -no-audio -no-cache
+#emulator -avd pixel_9.0 -port 5558 -no-window -no-audio -no-cache
+#emulator -avd $device -no-metrics -no-snapshot-load $*
 emulator -avd $device -no-metrics $*
